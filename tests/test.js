c3e020825a1e53a4a80fc6425c3b2be451607ab0
Santosh Rajan <santrajan@gmail.com>
2016-11-20 14:14:03 +0530

    adds programParser

--- tests/test.js
+++ tests/test.js
@@ -1,80 +1,67 @@
 const test = require('tape')
-const parser = require(__dirname + '/../lib/parser')
+const parser = require('../lib/parser')
 const fs = require('fs')
-let full_src = fs.readFileSync('test.cl', 'utf8').split('\n')
-// Variable Declaration Parser
-let src1 = full_src[0].toString()
-let expc1 = {
-    "type": "Program",
-    "body": [
+const src = fs.readFileSync('tests/test.cl', 'utf8').toString()
+const expected = {
+  'type': 'Program',
+  'body': [
+    {
+      'type': 'VariableDeclaration',
+      'declarations': [
         {
-            "type": "VariableDeclaration",
-            "declarations": [
-                {
-                    "type": "VariableDeclarator",
-                    "id": {
-                        "type": "Identifier",
-                        "name": "a"
-                    },
-                    "init": {
-                        "type": "Literal",
-                        "value": 7,
-                        "raw": "7"
-                    }
-                }
-            ],
-            "kind": "const"
+          'type': 'VariableDeclarator',
+          'id': {
+            'type': 'Identifier',
+            'name': 'a'
+          },
+          'init': {
+            'type': 'Literal',
+            'value': 7,
+            'raw': '7'
+          }
         }
-    ],
-    "sourceType": "script"
-}
-
-// Function Declaration Parser
-let src2 = full_src[1].toString()
-let expc2 = {
-    "type": "Program",
-    "body": [
+      ],
+      'kind': 'const'
+    },
+    {
+      'type': 'VariableDeclaration',
+      'declarations': [
         {
-            "type": "VariableDeclaration",
-            "declarations": [
-                {
-                    "type": "VariableDeclarator",
-                    "id": {
-                        "type": "Identifier",
-                        "name": "add"
-                    },
-                    "init": {
-                        "type": "ArrowFunctionExpression",
-                        "id": null,
-                        "params": [
-                            {
-                                "type": "Identifier",
-                                "name": "a"
-                            },
-                            {
-                                "type": "Identifier",
-                                "name": "b"
-                            }
-                        ],
-                        "body": {
-                            "type": "Literal",
-                            "value": 1,
-                            "raw": "1"
-                        },
-                        "generator": false,
-                        "expression": true
-                    }
-                }
+          'type': 'VariableDeclarator',
+          'id': {
+            'type': 'Identifier',
+            'name': 'f'
+          },
+          'init': {
+            'type': 'ArrowFunctionExpression',
+            'id': null,
+            'params': [
+              {
+                'type': 'Identifier',
+                'name': 'a'
+              },
+              {
+                'type': 'Identifier',
+                'name': 'b'
+              }
             ],
-            "kind": "const"
+            'body': {
+              'type': 'Literal',
+              'value': 1,
+              'raw': '1'
+            },
+            'generator': false,
+            'expression': true
+          }
         }
-    ],
-    "sourceType": "script"
+      ],
+      'kind': 'const'
+    }
+  ],
+  'sourceType': 'script'
 }
-
 
 test('parser', function (t) {
-  t.plan(2)
-  t.deepEqual(parser(src1), expc1, 'Varaible Declaration Parser')
-  t.deepEqual(parser(src2), expc2, 'Function Declaration Parser')
+  t.plan(1)
+  t.deepEqual(parser(src), expected, 'Program Parser')
 })
