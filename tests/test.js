1d4c628f424b4495d52cf527d183847e9d60ff3d
Angad Gill <angadpalsingh92@gmail.com>
2016-11-28 09:28:49 +0530

    Resolve Conflict

--- tests/test.js
+++ tests/test.js
@@ -1,99 +1,6 @@
 const test = require('tape')
 const parser = require('../lib/parser')
 const fs = require('fs')
-<<<<<<< HEAD
-const src = fs.readFileSync('tests/test.cl', 'utf8').toString()
-const expected = {
-  'type': 'Program',
-  'body': [
-    {
-      'type': 'VariableDeclaration',
-      'declarations': [
-        {
-          'type': 'VariableDeclarator',
-          'id': {
-            'type': 'Identifier',
-            'name': 'a'
-          },
-          'init': {
-            'type': 'BinaryExpression',
-            'operator': '+',
-            'left': {
-              'type': 'Literal',
-              'value': 7,
-              'raw': '7'
-            },
-            'right': {
-              'type': 'Literal',
-              'value': 2,
-              'raw': '2'
-            }
-          }
-        }
-      ],
-      'kind': 'const'
-    },
-    {
-      'type': 'VariableDeclaration',
-      'declarations': [
-        {
-          'type': 'VariableDeclarator',
-          'id': {
-            'type': 'Identifier',
-            'name': 'f'
-          },
-          'init': {
-            'type': 'ArrowFunctionExpression',
-            'id': null,
-            'params': [
-              {
-                'type': 'Identifier',
-                'name': 'a'
-              },
-              {
-                'type': 'Identifier',
-                'name': 'b'
-              }
-            ],
-            'body': {
-              'type': 'Literal',
-              'value': 1,
-              'raw': '1'
-            },
-            'generator': false,
-            'expression': true
-          }
-        }
-      ],
-      'kind': 'const'
-    },
-    {
-      'type': 'ExpressionStatement',
-      'expression': {
-        'type': 'CallExpression',
-        'callee': {
-          'type': 'Identifier',
-          'name': 'f'
-        },
-        'arguements': [
-          {
-            'type': 'Literal',
-            'value': 5,
-            'raw': '5'
-          },
-          {
-            'type': 'Literal',
-            'value': 1,
-            'raw': '1'
-          }
-        ]
-      }
-    }
-
-  ],
-  'sourceType': 'script'
-}
-=======
 const src = fs.readFileSync('test.cl', 'utf8').toString()
 const estemplate = require('../lib/template')
 let expected = estemplate.ast()
@@ -107,7 +14,6 @@ expected.body = [
   estemplate.lambda([estemplate.identifier('a'), estemplate.identifier('b')],
     estemplate.literal('6'))
 ]
->>>>>>> ebcd8e7d79c7f615bc3ea0d310093cea91b22d54
 
 test('parser', t => {
   t.plan(1)
