63f22b20ca80ecae6a1770ea24c71bde975ed19e
Angad Gill <angadpalsingh92@gmail.com>
2017-03-16 14:31:30 +0530

    Update test files

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,163 @@
-const IO = require('io-square-node');
+
+const readline = require('readline')
+const fs = require('fs')
+
+const rlConfig = {
+  input: process.stdin,
+  output: process.stdout
+} /* Config for readline interface */
+
+/* IO class starts here */
+class IO {
+
+  constructor (ioFunc) {
+    this.then = cb => ioFunc((...args) => { cb(...args) })
+  }
+
+  reject (pred) {
+    let saveThen = this.then
+    this.then = cb => {
+      saveThen((...args) => {
+        let result = pred(...args)
+        if (result !== null) {
+          if (Array.isArray(result)) {
+            cb(...result)
+          } else {
+            cb(result)
+          }
+        }
+      })
+    }
+    return this
+  }
+
+  mayBeFalse (mv, handler) {
+    let saveThen = this.then
+    this.then = cb => {
+      saveThen((...args) => {
+        let result = mv(...args)
+        if (result === false) {
+          handler(...args)
+        } else {
+          cb(...args)
+        }
+      })
+    }
+    return this
+  }
+
+  mayBeNull (mv, handler) {
+    let saveThen = this.then
+    this.then = cb => {
+      saveThen((...args) => {
+        let result = mv(...args)
+        if (result === null) {
+          handler(...args)
+        } else {
+          cb(...args)
+        }
+      })
+    }
+    return this
+  }
+
+  mayBeErr (mv, handler) {
+    let saveThen = this.then
+    this.then = cb => {
+      saveThen((...args) => {
+        let result = mv(...args)
+        if (result instanceof Error) {
+          handler(...args)
+        } else {
+          cb(...args)
+        }
+      })
+    }
+    return this
+  }
+
+  mayBeTrue (mv, handler) {
+    let saveThen = this.then
+    this.then = cb => {
+      saveThen((...args) => {
+        let result = mv(...args)
+        if (result === true) {
+          handler(...args)
+        } else {
+          cb(...args)
+        }
+      })
+    }
+    return this
+  }
+
+  map (transform) {
+    let saveThen = this.then
+    this.then = cb => {
+      saveThen((...args) => {
+        let result = transform(...args)
+        if (Array.isArray(result)) {
+          cb(...result)
+        } else {
+          cb(result)
+        }
+      })
+    }
+    return this
+  }
+
+  bind (ioFunc) {
+    let saveThen = this.then
+    this.then = cb => {
+      saveThen((...args) => {
+        let io = ioFunc(...args)
+        io.then((...ioargs) => cb(...args, ...ioargs))
+      })
+    }
+    return this
+  }
+
+  static timer (s) {
+    var intervalId
+    var timer = new IO(cb => {
+      intervalId = setInterval(cb, Math.floor(s * 1000))
+    })
+    timer.clear = () => clearInterval(intervalId)
+    return timer
+  }
+
+  static createIO (ioFunc) {
+    return new IO(ioFunc)
+  }
+
+  static getLine (str) {
+    const rl = readline.createInterface(rlConfig)
+    return new IO(cb => rl.question(str, cb))
+      .map(data => {
+        rl.close()
+        return data
+      })
+  }
+
+  static putLine (...data) {
+    return new IO(cb => process.nextTick(cb, data))
+      .map(data => {
+        console.log(...data)
+        return data
+      })
+  }
+
+  static readFile (filename) {
+    return new IO(cb => fs.readFile(filename, cb))
+      .map((_, data) => data.toString())
+  }
+
+  static writeFile (filename, data) {
+    return new IO(cb => fs.writeFile(filename, data, cb))
+  }
+}
+
+const request = require('request');
 const fact = n => {
     switch (n) {
     case 1:
@@ -11,16 +170,11 @@ const computeFact = IO.getLine('enter value for factorial: ').map(num => [
     fact(parseInt(num)),
     num
 ]).bind((val, num) =>
-    (IO.putLine(val))).map((val, num) => [val]);
-const getAscii = computeFact.map(num => [
-    String(num),
-    num
-]).bind((strNum, num) => IO.httpGet('http://artii.herokuapp.com/make?text=' + strNum)).map((strNum, num, ascii) => [
-    ascii,
-    strNum,
-    num,
-    ascii
-]).bind((art, strNum, num, ascii) =>
-    (IO.putLine(art))).map((art, strNum, num, ascii) => [[]]);
+    (IO.putLine(val))).map((val, num) => [String(val)]);
+const getAscii = computeFact.map(data => [
+    'http://artii.herokuapp.com/make?text=' + data,
+    data
+]).bind((link, data) => IO.createIO(cb => request(link, cb))).mayBeErr((link, data, err, res, body) => err, (link, data, err, res, body) => IO.putLine(err).then(() => null)).bind((link, data, err, res, body) =>
+    (IO.putLine(body))).map((link, data, err, res, body) => [body]);
 const tempIO = getAscii;
 const main = tempIO.then(() => null);
