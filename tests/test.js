4277e01be3ae86bb28d285de76d04332d3f907ee
Mrinal Purohit <github@mrinalpurohit.in>
2017-03-30 12:58:24 +0530

    Update generated `js` file

--- tests/test.js
+++ tests/test.js
@@ -9,15 +9,23 @@ const fact = n => {
         return n * fact(n - 1);
     }
 };
-const computeFact = IO.getLine('enter value for factorial: ').map(num => [
-    fact(parseInt(num)),
-    num
-]).bind((val, num) =>
-    (IO.putLine(val))).map((val, num) => [String(val)]);
-const getAscii = computeFact.map(data => [
-    'http://artii.herokuapp.com/make?text=' + data,
-    data
-]).bind((link, data) => IO.createIO(cb => request(link, cb))).maybeErr((link, data, err, res, body) => err, (link, data, err, res, body) => IO.putLine(err).then(() => null)).bind((link, data, err, res, body) =>
-    (IO.putLine(body))).map((link, data, err, res, body) => [body]);
+const computeFact = () =>
+    (IO.getLine('enter value for factorial: ').map(num => [num]).map(num => [
+        fact(parseInt(num)),
+        num
+    ]).bind((val, num) =>
+        (IO.putLine(val))).map((val, num) => [String(val)]));
+const getAscii = () =>
+    (computeFact().map(data => [
+        'http://artii.herokuapp.com/make?text=' + data,
+        data
+    ]).bind((link, data) => IO.createIO(cb => request(link, cb))).map((link, data, err, res, body) => [
+        link,
+        data,
+        err,
+        res,
+        body
+    ]).maybeErr((link, data, err, res, body) => err, (link, data, err, res, body) => IO.putLine(err).then(() => null)).bind((link, data, err, res, body) =>
+        (IO.putLine(body))).map((link, data, err, res, body) => [body]));
 const tempIO = getAscii;
-const main = tempIO.then(() => null);
+const main = tempIO().then(() => null);
